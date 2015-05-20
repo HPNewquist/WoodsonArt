bfafd977a02f30f53471dba5c2732cc7af0f450a
AJWGeek <austin@ajwgeek.com>
2015-05-20 02:02:57 -0500

    Tweaks

--- www/assets/app.js
+++ www/assets/app.js
@@ -1,13 +1,15 @@
 var app = angular.module('WoodsonArt', ['ionic', 'ngCordova']); /* ngCordova provides PhoneGap API access! */
 var currentTour = 0;
 var loadedTourFile;
+var storedTourSelectorData;
 
 app.config(function($stateProvider, $urlRouterProvider)
 {
   $stateProvider.state('index', { url: '/home', templateUrl: 'home.html', controller: indexController });
   $stateProvider.state('information', { url: '/information', templateUrl: 'information.html', controller: informationController });
   $stateProvider.state('tourSelector', { url: '/tourSelector', templateUrl: 'tourSelector.html', controller: tourSelectorController, cache: true});
-  $stateProvider.state('artworkSelector', { url: '/artworkSelector', templateUrl: 'artworkSelector.html', controller: artworkSelectorController, cache: false});
+  $stateProvider.state('artworkSelectorTable', { url: '/artworkSelectorTable', templateUrl: 'artworkSelectorTable.html', controller: artworkSelectorTableController, cache: false});
+  $stateProvider.state('artworkSelectorGrid', { url: '/artworkSelectorGrid', templateUrl: 'artworkSelectorGrid.html', controller: artworkSelectorGridController, cache: false});
   $urlRouterProvider.otherwise('/home'); /* Redirect the app to the home page on launch. */
 });
 
@@ -47,7 +49,8 @@ function loadTourSelectorData($scope)
        {
          if(tourSelectorData.pages[i].id == currentTour)
          {
-           $scope.objects = tourSelectorData.pages[i].content;
+           storedTourSelectorData = tourSelectorData.pages[i].content;
+           $scope.objects = storedTourSelectorData;
          }
          else
          {
@@ -105,12 +108,38 @@ function tourSelectorController($scope, $ionicHistory, $state)
     if(isNaN($link))
     {
       loadedTourFile = $link;
-      $state.go("artworkSelector");
+      for(var i = 0; i < storedTourSelectorData.length; i++)
+      {
+        if(storedTourSelectorData[i].link == $link)
+        {
+          if(storedTourSelectorData[i].display == "table")
+          {
+            $state.go("artworkSelectorTable");
+          }
+          else if(storedTourSelectorData[i].display == "grid")
+          {
+            $state.go("artworkSelectorGrid");
+          }
+        }
+        else
+        {
+          continue;
+        }
+      }
     }
   };
 }
 
-function artworkSelectorController($scope, $ionicHistory)
+function artworkSelectorTableController($scope, $ionicHistory)
+{
+  loadTourData(loadedTourFile, $scope);
+  $scope.goBack = function()
+  {
+    goBack($ionicHistory);
+  };
+}
+
+function artworkSelectorGridController($scope, $ionicHistory)
 {
   loadTourData(loadedTourFile, $scope);
   $scope.goBack = function()
